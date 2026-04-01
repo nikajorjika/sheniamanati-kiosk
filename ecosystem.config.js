22a0a56a0583d8f2dcc5c23ec8c9ed27eeafe37c
Nika Jorjoliani <nikajorjika1993@gmail.com>
2026-04-02 01:07:17 +0400

    feat(ecosystem): add ecosystem configuration for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "kiosk",
+      cwd: "/var/www/kiosk.sheniamanati.ge",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
